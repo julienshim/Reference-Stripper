28a496f950c8c274a16a7545256c31506519b45a
Julien Shim <25723428+julienshim@users.noreply.github.com>
2019-09-13 23:39:26 -0700

    version 1.0

--- Reference Stripper/ReferenceStripper.js
+++ Reference Stripper/ReferenceStripper.js
@@ -1,77 +1,120 @@
+const Title = ({ title }) => <h1>{title}</h1>;
+
+const Count = ({ length }) => <div id="count">{length}</div>;
+
+const ConfirmButton = ({ className, text }) => (
+  <div id="confirm" className={className}>
+    {text}
+  </div>
+);
+
 class ReferenceStripper extends React.Component {
   constructor(props) {
-   super(props);
-   this.state = {
-    title: "Reference Stripper - Current Updating as of 5:07PM",
-    input: "",
-    output: "Click on this text to copy.",
-    copied: false
-   };
+    super(props);
+    this.state = {
+      title: "Reference Stripper",
+      input: "",
+      output: "Click on this text to copy.",
+      copied: false,
+      flicker: false
+    };
   }
- 
+
   handleChange = (value, type) => {
-   this.setState({ [type]: value }, () => {
-    if (type === "input") { this.handleStrip(this.state.input);
-    }
-   });
+    this.setState({ [type]: value }, () => {
+      if (type === "input") {
+        this.handleStrip(this.state.input);
+      }
+    });
   };
- 
- handleClear = () => {
-  this.setState({copied: false})
- }
- 
+
+  handleClear = () => {
+    this.setState({ copied: false });
+  };
+
+  handleFlicker = () => {
+    this.setState({ flicker: true });
+    const timer = setTimeout(() => {
+      this.setState({ flicker: false });
+    }, 75);
+
+    return () => clearTimeout(timer);
+  };
+
   handleStrip = string => {
-   let isWriting = true;
-   let isQuoting = false;
-   let hasClosed = true;
-   let stripped = "";
-   for (let i = 0; i < string.length; i++) {
-    if (string[i] === "[" && !isQuoting) { isWriting = false; hasClosed = false}
-    if (string[i] ==="\"") { isQuoting = !isQuoting; }
-    if (string[i] === "]") { hasClosed = true }
-    if (string[i] === " " && hasClosed ) { isWriting = true;}
-    if (isWriting) { stripped += string[i] }
-    console.log(isWriting)
-   }
-   this.handleChange(stripped, "output");
+    let isWriting = true;
+    let isQuoting = false;
+    let hasClosed = true;
+    let stripped = "";
+    for (let i = 0; i < string.length; i++) {
+      if (string[i] === "[" && !isQuoting) {
+        isWriting = false;
+        hasClosed = false;
+      }
+      if (string[i] === '"') {
+        isQuoting = !isQuoting;
+      }
+      if (string[i] === "]") {
+        hasClosed = true;
+      }
+      if (string[i] === " " && hasClosed) {
+        isWriting = true;
+      }
+      if (isWriting) {
+        stripped += string[i];
+      }
+      console.log(isWriting);
+    }
+    this.handleChange(stripped, "output");
   };
- 
-   onCopy = () => {
-     this.setState({copied: true});
-   };
- 
+
+  onCopy = () => {
+    this.setState({ copied: true });
+  };
+
   render() {
-   return (
-    <div id="container">
-     <div id="header">
-      <h1>{this.state.title}</h1>
-      <div id="count">
-       {this.state.input.length + " → " + this.state.output.length}
+    const flickr =
+      this.state.flicker && this.state.copied
+        ? "flicker red"
+        : this.state.copied
+        ? "red"
+        : "";
+
+    return (
+      <div id="container">
+        <Title title={this.state.title} />
+        <div id="main">
+          <div id="editor">
+            <textarea
+              id="input"
+              value={this.state.input}
+              placeholder="Paste original Wikipedia text here."
+              onClick={this.handleClear}
+              onChange={event => {
+                this.handleChange(event.target.value, "input");
+              }}
+            />
+            <Count length={this.state.input.length} />
+          </div>
+          <CopyToClipboard onCopy={this.onCopy} text={this.state.output}>
+            <div id="preview" onClick={this.handleFlicker}>
+              <textarea
+                id="output"
+                value={this.state.output}
+                class={flickr}
+                readonly
+              />
+              <Count length={this.state.output.length} />
+              <ConfirmButton
+                className={this.state.copied ? "red confirm" : "confirm"}
+                text={this.state.copied ? "Copied!" : "Copy"}
+              />
+            </div>
+          </CopyToClipboard>
+        </div>
       </div>
-     </div>
-     <div id="main">
-      <div id="editor">
-      <textarea
-       id="input"
-       value={this.state.input}
-       placeholder="Paste original Wikipedia text here."
-       onClick={this.handleClear}
-       onChange={event => {
-        this.handleChange(event.target.value, "input");
-       }}
-      />
-      </div>
-      <div id="preview">
-       <CopyToClipboard onCopy={this.onCopy} text={this.state.output}>
-        <textarea id="output" class={this.state.copied && 'red'} value={this.state.output} readonly />
-      </CopyToClipboard>
-      <div id="confirm">{this.state.copied ? "Copied!" : "Copy"}</div>
-      </div>
-      </div>
-    </div>
-   );
+    );
   }
- }
- 
- ReactDOM.render(<ReferenceStripper />, document.getElementById("app"));
- 
+}
+
+ReactDOM.render(<ReferenceStripper />, document.getElementById("app"));
