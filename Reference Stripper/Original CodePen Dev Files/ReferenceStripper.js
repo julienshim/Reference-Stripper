f093b856bc7ad92e351d4ba6c1e272b95f676aaa
Julien Shim <25723428+julienshim@users.noreply.github.com>
2019-10-16 19:36:57 -0700

    accounts for rare [5]:24--2 like formatting

--- Reference Stripper/Original CodePen Dev Files/ReferenceStripper.js
+++ Reference Stripper/Original CodePen Dev Files/ReferenceStripper.js
@@ -3,19 +3,6 @@
 // Created by Julien Shim on 9/11/19.
 // Copyright © 2019 Julien Shim. All rights reserved.
 
-const ToggleButton = ({ isEasy, handleSettingsMode }) => (
-  // <div>
-  //  <button onClick={handleSettingsMode}>{isEasy ? "Easy" : "Advanced"}</button>
-  // </div>
-  <div id="toggle">
-    <label class="switch">
-      <input checked={isEasy} onClick={handleSettingsMode} type="checkbox" />
-      <span class="slider"></span>
-    </label>
-    <p id="toggleLabel">{isEasy ? "Easy Mode \(BETA\)" : "Split Mode"}</p>
-  </div>
-);
-
 const CircularProgressBar = ({ wordCount, size }) => {
   const percentage = (wordCount / 30) * 100;
   const radius = size;
@@ -79,54 +66,69 @@ const CircularProgressBar = ({ wordCount, size }) => {
   );
 };
 
-const Title = ({ title }) => <h1>{title}</h1>;
-
-const Count = ({ length, wordCount }) => (
-  <div id="count">{length + " characters / " + wordCount + " words"}</div>
+const ConfirmButton = ({ className, label, handleCopy, style }) => (
+  <div id="confirm" className={className} onClick={handleCopy} style={style}>
+    {label}
+  </div>
 );
 
-const ConfirmButton = ({ className, text, onCopy, label, handleFlicker }) => (
-  <CopyToClipboard onCopy={onCopy} text={text}>
-    <div id="confirm" className={className} onClick={handleFlicker}>
-      {label}
+const Title = ({ text, style }) => <h1 style={style}>{text}</h1>;
+
+const ToggleButton = ({ isDark, handleToggleDarkMode, style }) => (
+  <div id="toggle">
+    <label className="switch">
+      <input
+        defaultChecked={isDark}
+        type="checkbox"
+        onClick={handleToggleDarkMode}
+      />
+      <span className="slider"></span>
+    </label>
+    <p id="toggleLabel" style={style}>
+      {"Dark Mode"}
+    </p>
+  </div>
+);
+
+const Wrapper = ({ children, style }) => {
+  return (
+    <div id="wrapper" style={style}>
+      {children}
     </div>
-  </CopyToClipboard>
-);
+  );
+};
 
 class ReferenceStripper extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      title: "Reference Stripper",
+      title: "",
       input: "",
       output: "",
       copied: false,
-      isEasy: false,
-      start: 0,
-      end: 0,
+      isDark: false
     };
   }
 
+  componentDidMount() {
+    const sample =
+      "Lorem ipsum sit amet[1], consectetur elit[citation needed], sed do tempor ut labore (https://en.wikipedia.org/wiki/Lorem_ipsum)[2], dolore (https://en.wikipedia.org/wiki/Lorem_ipsum) magna aliqua (https://en.wikipedia.org/wiki/Lorem_ipsum) ultrices sagittis orci.[3] Ut imperdiet iaculus rhoncus, placerat quam, ut vehicula pulvinar.[5]:35 Fusce vestibulum[10]:400,418[11][12][13][14], et ”mattis orci iaculis!”.[5]:35–36";
+    this.setState(
+      {
+        title: "Reference Stripper"
+      },
+      () => {
+        this.handleChange(sample, "input");
+      }
+    );
+  }
+
   handleChange = (value, type) => {
-    console.log("before", this.input.selectionStart, this.input.selectionEnd);
-    if (type === "input") {
-     this.setState({start: this.input.selectionStart, end: this.input.selectionEnd});
-    }
     this.setState({ [type]: value, copied: false }, () => {
-     // console.log("after", this.state.start, this.state.end);
       if (type === "input") {
         this.handleChange(this.handleStrip(this.state.input), "output");
-
       }
-                  this.input.setSelectionRange(this.state.start, this.state.end);
-
     });
-  };
-
-  handleSettingsMode = () => {
-    this.setState(prevState => ({
-      isEasy: !prevState.isEasy
-    }));
   };
 
   handleFlicker = () => {
@@ -134,15 +136,19 @@ class ReferenceStripper extends React.Component {
     const timer = setTimeout(() => {
       this.setState({ flicker: false });
     }, 75);
-
     return () => clearTimeout(timer);
   };
 
   handleWordCount = string => {
-   // Regex Replace takes in account copy and pasted lists
-    return string.replace(/[\n]/g, " ").split(" ").filter(function(n) {
-      return n != "";
-    }).length;
+    return (
+      string
+        // strips newline before counting
+        .replace(/[\n]/g, " ")
+        .split(" ")
+        .filter(function(n) {
+          return n != "";
+        }).length
+    );
   };
 
   handleStrip = string => {
@@ -150,12 +156,14 @@ class ReferenceStripper extends React.Component {
     let isQuoting = false;
     let hasClosed = true;
     let stripped = "";
+    let isReferencing = false;
     for (let i = 0; i < string.length; i++) {
       if (string[i] === "[" && !isQuoting) {
         isWriting = false;
         hasClosed = false;
+        isReferencing = true;
       }
-      // this saves the trouble of removing addresses when copying hyperlinked text
+      // removes embedded (<url)
       if (
         string[i + 1] === "(" &&
         string[i + 2] === "h" &&
@@ -175,27 +183,48 @@ class ReferenceStripper extends React.Component {
       }
       if (string[i] === "]") {
         hasClosed = true;
+        if (string[i + 1] !== ":") {
+          isReferencing = false;
+        }
       }
-      if ((string[i] === " " ||
-           string[i] === "," ||
-           string[i] === "." ||
-           string[i] === "!" ||
-           string[i] === ";" ||
-           string[i] === ":" ||
-           string[i] === "?"
-          ) && hasClosed) {
+      if (string[i + 1] === " " && isReferencing) {
+        isReferencing = !isReferencing;
+      }
+      if (
+        (string[i] === " " ||
+          string[i] === "," ||
+          string[i] === "." ||
+          string[i] === "!" ||
+          string[i] === ";" ||
+          string[i] === ":" ||
+          string[i] === "?") &&
+        hasClosed &&
+        !isReferencing
+      ) {
         isWriting = true;
       }
       if (isWriting) {
         stripped += string[i];
       }
     }
-    // this.handleChange(stripped, "output");
     return stripped;
   };
 
-  onCopy = () => {
-    this.setState({ copied: true });
+  outputTextareaRef = React.createRef();
+
+  handleCopy = event => {
+    const textareaText = this.outputTextareaRef.current;
+    textareaText.select();
+    document.execCommand("copy");
+    this.setState({ copied: true }, () => {
+      this.handleFlicker();
+    });
+  };
+
+  handleToggleDarkMode = () => {
+    this.setState(prevState => ({
+      isDark: !prevState.isDark
+    }));
   };
 
   render() {
@@ -206,89 +235,68 @@ class ReferenceStripper extends React.Component {
         ? "red"
         : "";
 
-    const view = this.state.isEasy ? "easy-view" : "split-view";
+    const themeDark = {
+      background: "var(--charcoal)",
+      color: this.state.copied ? "" : "var(--ash)"
+    };
 
-    const placeholder =
-      "Lorem ipsum dolor sit amet[1], consectetur adipiscing elit[citation needed], sed do eiusmod tempor incididunt ut labore et (https://en.wikipedia.org/wiki/Lorem_ipsum)[2], dolore (https://en.wikipedia.org/wiki/Lorem_ipsum) magna aliqua (https://en.wikipedia.org/wiki/Lorem_ipsum) ultrices sagittis orci a.[3]";
+    const theme = this.state.isDark ? themeDark : {};
 
     return (
-      <div id="container">
-        <div id="header">
-          <Title title={this.state.title} />
-          <ToggleButton
-            isEasy={this.state.isEasy}
-            handleSettingsMode={this.handleSettingsMode}
-          />
-        </div>
-        <div id="main">
-          <div id="editor">
-            <textarea
-              id="input"
-              value={this.state.isEasy ? this.state.output : this.state.input}
-              placeholder={
-                this.state.isEasy ? this.handleStrip(placeholder) : placeholder
-              }
-              class={`${view} ${this.state.isEasy && flickr}`}
-              ref={ref => this.input = ref}
-              onChange={event => {
-               console.log(event.target.value); this.handleChange(event.target.value, "input");
-              }}
+      <Wrapper isDark={this.state.isDark} style={theme}>
+        <div id="container">
+          <div id="header">
+            <Title
+              text={this.state.title}
+              style={{ ...theme, color: this.state.isDark ? "var(--ash)" : "" }}
             />
-            {this.state.isEasy && (
-              <CircularProgressBar
-                wordCount={
-                  this.state.output === ""
-                    ? 23
-                    : this.handleWordCount(
-                        this.state.isEasy ? this.state.output : this.state.input
-                      )
-                }
-                size={25}
-              />
-            )}
-            {/* <Count length={this.state.input.length} wordCount={this.handleWordCount(this.state.input)} /> */}
-            {this.state.isEasy && (
-              <ConfirmButton
-                className={this.state.copied ? "red confirm" : "confirm"}
-                onCopy={this.onCopy}
-                text={this.state.output}
-                label={this.state.copied ? "Copied!" : "Copy"}
-                handleFlicker={this.handleFlicker}
-              />
-            )}
-                     {this.state.isEasy && <p className="warning">Warning: Any edits to text while in easy mode will irreversibly strip all references <span class="example"> e.g. [3]</span>, hyperlink URLs <span className ="example"> e.g. (https://en.wikipedia.org/wiki/Lorem_ipsum)</span>, and may also delete needed text (<em>if omitted by the current version stripping algorithm from view</em>) from the original inputted text. Use split mode if being able to view the original inputted text and stripped text side by side is critical to your work.</p>}
+            <ToggleButton
+              isDark={this.state.isDark}
+              handleToggleDarkMode={this.handleToggleDarkMode}
+              style={{ ...theme, color: this.state.isDark ? "var(--ash)" : "" }}
+            />
           </div>
-
-          {!this.state.isEasy && (
-            <CopyToClipboard onCopy={this.onCopy} text={this.state.output}>
-            <div id="preview" onFocus={this.handleFlicker}>
+          <div id="main">
+            <div id="editor">
               <textarea
-                id="output"
-                placeholder={this.handleStrip(placeholder)}
-                value={this.state.output}
-                class={`${view} ${flickr}`}
-                readonly
-              />
-              <CircularProgressBar
-                wordCount={
-                  this.state.input === ""
-                    ? 23
-                    : this.handleWordCount(this.state.output)
-                }
-                size={25}
-              />
-              <ConfirmButton
-                className={this.state.copied ? "red confirm" : "confirm"}
-                onCopy={this.onCopy}
-                text={this.state.output}
-                label={this.state.copied ? "Copied!" : "Copy"}
-                handleFlicker={this.handleFlicker}
+                id="input"
+                value={this.state.input}
+                className={"split-view"}
+                ref={ref => (this.input = ref)}
+                onChange={event => {
+                  this.handleChange(event.target.value, "input");
+                }}
+                style={{
+                  ...theme,
+                  color: this.state.isDark ? "var(--ash)" : ""
+                }}
               />
             </div>
-           </CopyToClipboard>
-          )}
+            <div id="preview">
+              <textarea
+                id="output"
+                value={this.state.output}
+                className={`split-view ${flickr}`}
+                ref={this.outputTextareaRef}
+                onFocus={this.handleCopy}
+                style={theme}
+                readOnly
+              />
+              <CircularProgressBar
+                wordCount={this.handleWordCount(this.state.output)}
+                size={25}
+              />
+              <ConfirmButton
+                className={this.state.copied ? "red confirm" : "confirm"}
+                text={this.state.output}
+                label={this.state.copied ? "Copied!" : "Copy"}
+                handleCopy={this.handleCopy}
+                style={theme}
+              />
+            </div>
+          </div>
         </div>
-      </div>
+      </Wrapper>
     );
   }
 }
