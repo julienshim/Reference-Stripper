bea67dee2c9f2e21fd4d8c19f55791b425fa57ee
Julien Shim <25723428+julienshim@users.noreply.github.com>
2019-11-26 11:16:59 -0800

    proptypes, eslint, prettier

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
 
-export default ({ value, isDark, placeholder, handleChange, changelogRef }) => (
+const Input = ({ value, isDark, placeholder, handleChange, changelogRef }) => (
   <textarea
     id="input"
     value={value}
-    className={`split-view ${isDark ? "dark" : ""}`}
+    className={`split-view ${isDark ? 'dark' : ''}`}
     placeholder={placeholder}
     ref={changelogRef}
     onChange={handleChange}
   />
 );
+
+Input.propTypes = {
+  value: PropTypes.string,
+  isDark: PropTypes.bool,
+  placeholder: PropTypes.string,
+  handleChange: PropTypes.func,
+  changelogRef: PropTypes.oneOfType([
+    PropTypes.func,
+    PropTypes.shape({
+      current: PropTypes.any
+    })
+  ])
+};
+
+export default Input;
