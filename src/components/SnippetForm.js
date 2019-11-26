3b96684005bfda48985e612dcad7d38d1dbc4053
Julien Shim <25723428+julienshim@users.noreply.github.com>
2019-11-26 11:18:18 -0800

    proptypes, eslint, prettier

--- src/components/SnippetForm.js
+++ src/components/SnippetForm.js
@@ -1,17 +1,27 @@
-import React from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
 
-export default ({ value, isDark, handleChange, onSubmit }) => {
+const SnippetForm = ({ value, isDark, handleChange, onSubmit }) => {
   return (
     <form id="snippetForm" onSubmit={onSubmit}>
       <input
-        className={isDark ? "dark" : ""}
+        className={isDark ? 'dark' : ''}
         type="text"
         id="snippetInput"
         value={value}
         placeholder="Enter new snippet"
         autoComplete="off"
         onChange={handleChange}
-      ></input>
+      />
     </form>
   );
 };
+
+SnippetForm.propTypes = {
+  value: PropTypes.string,
+  isDark: PropTypes.bool,
+  handleChange: PropTypes.func,
+  onSubmit: PropTypes.func
+};
+
+export default SnippetForm;
