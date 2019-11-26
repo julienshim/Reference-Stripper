d23caaa3d25746a1ca64d1f0900e4a3c0382ba5d
Julien Shim <25723428+julienshim@users.noreply.github.com>
2019-11-26 11:14:57 -0800

    proptypes, eslint, prettier

--- src/components/ConfirmButton.js
+++ src/components/ConfirmButton.js
@@ -1,13 +1,38 @@
-import React from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
 
-export default ({ className, label, handleCopy, isDark, copied }) => {
+const ConfirmButton = ({ className, input, handleCopy, isDark, copied }) => {
   const themeDark = {
-    color: copied ? "" : "var(--ash)"
+    color: copied ? '' : 'var(--ash)'
   };
   const theme = isDark ? themeDark : {};
+  let label;
+  if (copied) {
+    label = input !== '' ? 'Copied' : 'Nothing to Copy!';
+  } else {
+    label = 'Copy';
+  }
   return (
-    <div id="confirm" className={className} onClick={handleCopy} style={theme}>
+    <div
+      id="confirm"
+      className={className}
+      onClick={handleCopy}
+      onKeyUp={handleCopy}
+      style={theme}
+      role="button"
+      tabIndex={0}
+    >
       {label}
     </div>
   );
 };
+
+ConfirmButton.propTypes = {
+  className: PropTypes.string,
+  input: PropTypes.string,
+  handleCopy: PropTypes.func,
+  isDark: PropTypes.bool,
+  copied: PropTypes.bool
+};
+
+export default ConfirmButton;
