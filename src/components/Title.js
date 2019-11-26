849402427b2f8bb7b5eeaad254b82cf2670c0827
Julien Shim <25723428+julienshim@users.noreply.github.com>
2019-11-26 11:19:16 -0800

    eslint, proptypes, prettier

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
 
-export default ({ text, isDark }) => (
-  <h1 id="title" className={isDark ? "dark" : ""}>
+const Title = ({ text, isDark }) => (
+  <h1 id="title" className={isDark ? 'dark' : ''}>
     {text}
   </h1>
 );
+
+Title.propTypes = {
+  text: PropTypes.string,
+  isDark: PropTypes.bool
+};
+
+export default Title;
