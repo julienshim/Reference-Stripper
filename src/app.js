d47d44641391fc4385451940e82c9bd476fb7b59
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-04-10 09:58:52 -0700

    single quotes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 import ReferenceStripper from './components/ReferenceStripper';
-import "normalize.css/normalize.css";
-import "./styles/styles.scss";
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
 
-ReactDOM.render(<ReferenceStripper />, document.getElementById("app"));
+ReactDOM.render(<ReferenceStripper />, document.getElementById('app'));
