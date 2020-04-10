beab78441553acf7f62e21d65b59cb1d9c254943
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-04-10 16:25:36 -0700

    title pointer + blue color on lowercase mode

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,19 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Title = ({ text, isDark, handleCasing, isLowerCase }) => (
-  <div
-    onClick={handleCasing}
-    onKeyUp={handleCasing}
-    role="button"
-    tabIndex={0}
-    style={{ outline: 'none' }}
-  >
-    <h1 id="title" className={isDark ? 'dark' : ''}>
-      {isLowerCase ? text.toLowerCase() : text}
-    </h1>
-  </div>
-);
+const Title = ({ text, isDark, handleCasing, isLowerCase }) => {
+  let textColor;
+
+  if (isLowerCase) {
+    textColor = 'var(--blue)';
+  } else if (isDark) {
+    textColor = 'var(--ash)';
+  }
+
+  return (
+    <div
+      onClick={handleCasing}
+      onKeyUp={handleCasing}
+      role="button"
+      tabIndex={0}
+      style={{ outline: 'none' }}
+    >
+      <h1
+        id="title"
+        style={{ color: textColor }}
+        className={isDark ? 'dark' : ''}
+      >
+        {isLowerCase ? text.toLowerCase() : text}
+      </h1>
+    </div>
+  );
+};
 
 Title.propTypes = {
   text: PropTypes.string,
