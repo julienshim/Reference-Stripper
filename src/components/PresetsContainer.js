278e7b85af2a0345e0d2593bb6360ca45359054e
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-04-16 19:23:04 -0700

    new feature presets container

---
+++ src/components/PresetsContainer.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const PresetsContainer = ({ presets, isDark, handleLoadPreset }) => {
+  const buttonStyle = {
+    border: isDark ? '1px solid var(--blue)' : '1px solid var(--faded-blue)',
+    background: isDark ? 'var(--faded-clay' : 'var(--faded-ash)'
+  };
+  return (
+    <div id="presetsContainer">
+      <div
+        id="preset-first"
+        className="presets-button"
+        style={buttonStyle}
+        data-preset={presets.first}
+        onClick={() => handleLoadPreset('first')}
+        onKeyUp={() => handleLoadPreset('first')}
+        role="button"
+        tabIndex={0}
+      >
+        Preset 1
+      </div>
+      <div
+        id="preset-second"
+        className="presets-button"
+        style={buttonStyle}
+        data-preset={presets.second}
+        onClick={() => handleLoadPreset('second')}
+        onKeyUp={() => handleLoadPreset('second')}
+        role="button"
+        tabIndex={0}
+      >
+        Preset 2
+      </div>
+    </div>
+  );
+};
+
+PresetsContainer.propTypes = {
+  presets: PropTypes.shape({
+    first: PropTypes.string,
+    second: PropTypes.string
+  }),
+  isDark: PropTypes.bool,
+  handleLoadPreset: PropTypes.func
+};
+
+export default PresetsContainer;
