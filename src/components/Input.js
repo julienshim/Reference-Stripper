479a163ba44e312b596687959b88e98d0777cf61
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-08-30 10:40:16 -0700

    user defined special characters warning

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Input = ({ value, isDark, placeholder, handleChange, isLowerCase }) => {
-  return (
+const Input = ({
+  value,
+  isDark,
+  placeholder,
+  handleChange,
+  isLowerCase,
+  specialCharactersFound,
+}) => (
+  <div
+    style={{
+      position: 'relative',
+    }}
+  >
     <textarea
       id="input"
       value={isLowerCase ? value.toLowerCase() : value}
@@ -10,8 +21,17 @@ const Input = ({ value, isDark, placeholder, handleChange, isLowerCase }) => {
       placeholder={isLowerCase ? placeholder.toLowerCase() : placeholder}
       onChange={handleChange}
     />
-  );
-};
+    {specialCharactersFound.length > 0 && (
+      <div className="warning">
+        {specialCharactersFound.map((char, index) => (
+          <span key={char} style={{ margin: '4px', display: 'inline-flex' }}>
+            {char}
+          </span>
+        ))}
+      </div>
+    )}
+  </div>
+);
 
 Input.propTypes = {
   value: PropTypes.string,
@@ -19,6 +39,7 @@ Input.propTypes = {
   placeholder: PropTypes.string,
   handleChange: PropTypes.func,
   isLowerCase: PropTypes.bool,
+  specialCharactersFound: PropTypes.arrayOf(PropTypes.string),
 };
 
 export default Input;
